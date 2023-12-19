a396a04e0ba65e637723dc635fc66f54cedf4e39
GyancarlosPinto <gyancarlospinto@pursuit.org>
2023-12-19 05:13:39 -0500

    Completed Basic Introduction to React Router

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <img src="https://images.pexels.com/photos/1652394/pexels-photo-1652394.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        </div>
+    );
+}
+
+export default Home;
