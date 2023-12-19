a425ffb6cf26e6e80fc947055f5f0c8b813c0746
dttrue <danieltorres@pursuit.org>
2023-12-19 01:32:17 -0500

    Daniels Completed Basic Introduction to React Router

---
+++ src/component/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1>Welcome to the Home page</h1>
+            <img src='/imgs/Home.png' alt="Home" />
+        </div>
+    )
+}
+
+export default Home
