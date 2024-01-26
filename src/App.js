8b31539023104dd8af28378150fed16ec3c7f68f
SaraM09 <sayeramusfika@pursuit.org>
2024-01-26 17:53:13 -0500

    Sara's-03-21-exercise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import Home from './Components.js/home.js';
+import aboutPage from './Components.js/about';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to='/home'>Home</Link>
+                </li>
+                <li>
+                  <Link to='/about'>About</Link>
+                </li>
+              </ul>
+            </nav>
+            <Routes>
+            <Route path='/about' Component={aboutPage} />
+            <Route path='/home' Component={Home} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+   
   );
 }
 
---
+++ src/Components.js/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to the Home Page</h2>
+      <p>This is a simple message for the Home component.</p>
+      <img src="https://picsum.photos/200" alt="Random Image" />
+    </div>
+  );
+}
+
+export default Home;
