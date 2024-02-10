ef89d1622d83b5a93b8ff3b790bf36bc4585f0a8
MoniqueC10-4 <monique.charles13@gmail.com>
2024-02-10 11:14:30 -0500

    Completed Basic Introduction to React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; 
+import cozyhome from './cozyhome.webp'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <nav>
+            <Link to='/'>Home</Link><br />
+            <Link to='/about'>About</Link>
+          </nav>
+          <Routes>
+           <Route path='/' element={<Home />} />
+           <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </Router>  
+  );
+}
+function Home() {
+  return (
+    <>
+      <h1>Welcome to the Home Page</h1>
+      <img src={cozyhome} alt="Cozy Welcoming Home" />
+    </>
+  );
+}
+function About() {
+  return (
+    <>
+      <h1>Welcome to the About Page</h1>
+      <ol>
+        <li>The About link is clicked and React Router prevents the default action (full page reload)</li>
+        <li>The URL changes to match the destination ('/about') route path</li>
+        <li>React Router renders the component that corresponds to the route (the element prop tells the component to render)</li>
+        <li>React Router replaces the displayed component with the About component</li>
+      </ol>
+    </>
   );
 }
 
